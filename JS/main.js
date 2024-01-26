99df71bd16ec8c89e9818a0493927b599ea3ebf1
AlbertoCavans <alberto96cavani@gmail.com>
2024-01-26 14:08:38 +0100

    Feat JS - mini-imgs progress

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,8 @@ const sideColumn = document.querySelector(".side-column-slides");
 /* PROCESS */
 const imgs = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 
+const allMiniImgs = document.getElementsByClassName("mini-img");
+
 /* IMG CURRENTLY SHOWN */
 let currentIndex = 0;
 
@@ -28,13 +30,12 @@ imgsBoxLink.innerHTML = imgsHtml;
 /* ARROW DOWN */
 
 arrowDownLink.addEventListener("click", function () {
+  const oldMiniImg = allMiniImgs[currentIndex];
+  oldMiniImg.classList.remove("selected");
+
   const allImgs = document.getElementsByClassName("img");
-  /*   const allMiniSlides = document.getElementsByClassName("mini-slide"); */
   let prevImg = allImgs[currentIndex];
   prevImg.classList.remove("selected");
-
-  /*   const prevMiniSlide = allMiniSlides[currentIndex];
-  prevMiniSlide.classList.remove("selected"); */
 
   if (currentIndex >= allImgs.length - 1) {
     currentIndex = 0;
@@ -42,49 +43,100 @@ arrowDownLink.addEventListener("click", function () {
     currentIndex++;
   }
 
+  const newMiniImg = allMiniImgs[currentIndex];
+  newMiniImg.classList.add("selected");
+
   const nextImg = allImgs[currentIndex];
   nextImg.classList.add("selected");
 
-  /*   const nextMiniSlide = allMiniSlides[currentIndex];
-  nextMiniSlide.classList.add("selected"); */
+  /*   let miniImgHtml = "";
+  for (let i = 0; i < imgs.length; i++) {
+    const img = imgs[i];
+
+    let selectedClass;
+    if (i == currentIndex) {
+      selectedClass = "selected";
+    } else {
+      selectedClass = "";
+    }
+    miniImgHtml += `<div class="mini-slide">
+  <img src="./img/${img}" class="mini-img ${selectedClass}" alt="img 01" />
+</div>`;
+  }
+  sideColumn.innerHTML = miniImgHtml; */
 });
 
 /* ARROW UP */
 
-/* arrowUpLink.addEventListener("click", function () {
-  alert("vai alla precedente");
-}); */
-
 arrowUpLink.addEventListener("click", function () {
+  const oldMiniImg = allMiniImgs[currentIndex];
+  oldMiniImg.classList.remove("selected");
+
   const allImgs = document.getElementsByClassName("img");
-  /*   const allMiniSlides = document.getElementsByClassName("mini-slide"); */
   let prevImg = allImgs[currentIndex];
   prevImg.classList.remove("selected");
 
-  /*   const prevMiniSlide = allMiniSlides[currentIndex];
-  prevMiniSlide.classList.remove("selected");
- */
   if (currentIndex <= 0) {
     currentIndex = allImgs.length - 1;
   } else {
     currentIndex--;
   }
 
+  const newMiniImg = allMiniImgs[currentIndex];
+  newMiniImg.classList.add("selected");
+
   const nextImg = allImgs[currentIndex];
   nextImg.classList.add("selected");
 
-  /*   const nextMiniSlide = allMiniSlides[currentIndex];
-  nextMiniSlide.classList.add("selected"); */
+  /*   let miniImgHtml = "";
+  for (let i = 0; i < imgs.length; i++) {
+    const img = imgs[i];
+
+    let selectedClass;
+    if (i == currentIndex) {
+      selectedClass = "selected";
+    } else {
+      selectedClass = "";
+    }
+    miniImgHtml += `<div class="mini-slide">
+  <img src="./img/${img}" class="mini-img ${selectedClass}" alt="img 01" />
+</div>`;
+  }
+  sideColumn.innerHTML = miniImgHtml; */
 });
 
-/* ATTIVO LE SLIDE LATERALI */
-/* let miniSlideHtml = "";
+/* CREATE MINI-IMGS */
+let miniImgHtml = "";
 for (let i = 0; i < imgs.length; i++) {
   const img = imgs[i];
-  let selectedClass = i === currentIndex ? "selected" : "";
 
-  miniSlideHtml += `<div class="mini-slide ${selectedClass}">
-  <img src="./img/${img}" class="img" alt="img ${i}" />
-  </div>`;
+  let selectedClass;
+  if (i == currentIndex) {
+    selectedClass = "selected";
+  } else {
+    selectedClass = "";
+  }
+  miniImgHtml += `<div class="mini-slide"">
+  <img src="./img/${img}" class="mini-img ${selectedClass}" alt="img 01  data-selected-image="${i}"/>
+</div>`;
 }
-sideColumn.innerHTML = miniSlideHtml; */
+sideColumn.innerHTML = miniImgHtml;
+
+/* CLICK MINI-IMGS */
+
+/* for (let i = 0; i < allMiniImgs.length; i++) {
+  const miniImg = allMiniImgs[i];
+  miniImg.addEventListener("click", function () {
+    const oldMiniImg = allMiniImgs[currentIndex];
+    oldMiniImg.classList.remove("selected");
+
+    currentIndex = parseInt(miniImg.getAttribute("data-selected-image"));
+
+    const newMiniImg = allMiniImgs[currentIndex];
+    newMiniImg.classList.add("selected");
+
+    const img = imgs[currentIndex];
+    const imgsHtml = `<img src="./img/${img}" class="img ${selectedClass}" alt="img ${i}"  data-selected-image="${i} />`;
+    imgsBoxLink.innerHTML = imgsHtml;
+  });
+} */
